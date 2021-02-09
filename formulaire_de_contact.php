1c7f300044a978394809a19e20fd1a5eb9f1fff9
Theo Lemaigre <lemaigretheo@gmail.com>
2021-02-09 20:38:06 +0100

    Modif title d'accueil, ajouts dans head de contact

--- formulaire_de_contact.php
+++ formulaire_de_contact.php
@@ -31,9 +31,10 @@ if(isset($_POST['mailform']))
 }
 ?>
 
-<html>
+<html lang="fr">
 	<head>
 		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="contact.css"/>
 		<title>NetView - Contact</title>
 	</head>
