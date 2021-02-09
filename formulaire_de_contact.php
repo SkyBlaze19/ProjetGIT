7846c16599ab49987bec0ce354555873867a605c
Theo Lemaigre <lemaigretheo@gmail.com>
2021-02-09 21:14:26 +0100

    Restructuration de la page (header was in body)

--- formulaire_de_contact.php
+++ formulaire_de_contact.php
@@ -45,36 +45,37 @@ if(isset($_POST['mailform']))
 
 	</head>
 
-	<body>
-		<header>
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<a class="navbar-brand" href="#">Netflix Review</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">Netflix Review</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
 				<ul class="navbar-nav">
 					<li class="nav-item active">
-					<a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+						<a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
 					</li>
 					<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Reviews
-					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-						<a class="dropdown-item" href="#serie">séries</a>
-						<a class="dropdown-item" href="#film">films</a>
-						<a class="dropdown-item" href="#anime">animes</a>
-						<a class="dropdown-item" href="#documentaire">documentaires</a>
-					</div>
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Reviews
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+							<a class="dropdown-item" href="#serie">séries</a>
+							<a class="dropdown-item" href="#film">films</a>
+							<a class="dropdown-item" href="#anime">animes</a>
+							<a class="dropdown-item" href="#documentaire">documentaires</a>
+						</div>
 					</li>
 					<li class="nav-item">
-					<a class="nav-link" href="formulaire_de_contact.php">Contacts</a>
+						<a class="nav-link" href="formulaire_de_contact.php">Contacts</a>
 					</li>
 				</ul>
-				</div>
-			</nav>
-		</header>
+			</div>
+		</nav>
+	</header>
+
+	<body>
 		
 		<h2>Laissez nous un message !</h2>
 		
